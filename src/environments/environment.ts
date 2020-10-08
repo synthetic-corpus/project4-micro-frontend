aa3b33b2e4ef2eac12584d559390cd44e827d233
synthetic.corpus <joel@joelgonzaga.com>
2020-10-08 16:46:13 -0700

    corrected the API

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://cloudapi.joelgonzaga.com:80/api/v0'
+  apiHost: 'http://cloudapi.joelgonzaga.com:8080/api/v0'
 };
 
 /*
